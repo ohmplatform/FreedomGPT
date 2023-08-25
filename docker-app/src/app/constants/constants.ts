18432d84edf912a6ef8c68d73a662786a525108f
bhattaraijay05 <bhattaraijay23@gmail.com>
2023-08-25 11:45:57 +0545

    freedomgpt2.0

--- docker-app/src/app/constants/constants.ts
+++
@@ -1,3 +0,0 @@
-export const APPNAME = "FreedomGPT";
-export const BOTTOMTEXT =
-  "FreedomGPT Mar 22 Version: Free Research Preview. Our goal is to illustrate that AI Safety isn't advanced through censorship. Additionally, we believe every company and individual should have access to their own 100% private LLM. For more information reach out at contact@ageofai.capital";
--- docker-app/src/app/types/types.ts
+++
@@ -1,7 +0,0 @@
-export type MessageType = {
-  message: string;
-  user?: boolean;
-  image?: any;
-  id: string;
-  replyId?: string;
-};
